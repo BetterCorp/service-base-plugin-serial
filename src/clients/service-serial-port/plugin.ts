2de89747072f0fd27cc8a6386571375924d764b6
myusername <myemail@example.com>
2023-05-13 15:20:46 +0200

    feat(service-serial-port): add support for specifying serverId in onMessage, onPortEvent, writeMessage and reconnect methods

--- src/clients/service-serial-port/plugin.ts
+++ src/clients/service-serial-port/plugin.ts
@@ -10,6 +10,12 @@ import {
 } from "../../";
 import { Tools } from "@bettercorp/tools";
 
+export type SPOnMessage = {
+  (data: Buffer | string): Promise<void>;
+};
+export type SPOnEvent = {
+  (event: SerialPortEvent, meta?: any): Promise<void>;
+};
 export class serialPort extends ServicesClient<
   ServiceCallable,
   SerialEvents,
@@ -24,25 +30,63 @@ export class serialPort extends ServicesClient<
     if (Tools.isString(serverId)) this._serverId = serverId;
   }
 
-  async onMessage(listener: {
-    (data: Buffer | string): Promise<void>;
-  }): Promise<void> {
-    await this._plugin.onEventSpecific(this._serverId, "onMessage", listener);
+  public get defaultServer() {
+    return this._serverId;
   }
-  async onPortEvent(listener: {
-    (event: SerialPortEvent, meta?: any): Promise<void>;
-  }): Promise<void> {
-    await this._plugin.onEventSpecific(this._serverId, "onPortEvent", listener);
+  public set defaultServer(serverId: string) {
+    this._serverId = serverId;
   }
 
-  async writeMessage(data: Buffer | string): Promise<void> {
-    await this._plugin.emitEventAndReturnSpecific(
-      this._serverId,
-      "writeMessage",
-      data
+  async onMessage(listener: {
+    (data: Buffer | string): Promise<void>;
+  }): Promise<void>;
+  async onMessage(serverId: string, listener: SPOnMessage): Promise<void>;
+  async onMessage(
+    serverIdOrlistener: SPOnMessage | string,
+    listener?: SPOnMessage
+  ): Promise<void> {
+    await this._plugin.onEventSpecific(
+      listener === undefined ? this._serverId : (serverIdOrlistener as string),
+      "onMessage",
+      listener === undefined
+        ? (serverIdOrlistener as SPOnMessage)
+        : (listener as SPOnMessage)
     );
   }
-  async reconnect(): Promise<void> {
-    await this._plugin.emitEventAndReturnSpecific(this._serverId, "reconnect");
+
+  async onPortEvent(listener: SPOnEvent): Promise<void>;
+  async onPortEvent(serverId: string, listener: SPOnEvent): Promise<void>;
+  async onPortEvent(
+    serverIdOrlistener: SPOnEvent | string,
+    listener?: SPOnEvent
+  ): Promise<void> {
+    await this._plugin.onEventSpecific(
+      listener === undefined ? this._serverId : (serverIdOrlistener as string),
+      "onPortEvent",
+      listener === undefined
+        ? (serverIdOrlistener as SPOnEvent)
+        : (listener as SPOnEvent)
+    );
+  }
+
+  async writeMessage(data: Buffer | string): Promise<void>;
+  async writeMessage(serverId: string, data: Buffer | string): Promise<void>;
+  async writeMessage(
+    serverIdOrData: Buffer | string,
+    data?: Buffer | string
+  ): Promise<void> {
+    await this._plugin.emitEventAndReturnSpecific(
+      data === undefined ? this._serverId : (data as string),
+      "writeMessage",
+      data === undefined ? serverIdOrData : data
+    );
+  }
+  async reconnect(): Promise<void>;
+  async reconnect(serverId: string): Promise<void>;
+  async reconnect(serverId?: string): Promise<void> {
+    await this._plugin.emitEventAndReturnSpecific(
+      serverId === undefined ? this._serverId : serverId,
+      "reconnect"
+    );
   }
 }
